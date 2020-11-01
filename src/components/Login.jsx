f93678cd1c43154fbc8247348caf059b5c0d135f
K1C4Y <patryk.zajac.100@gmail.com>
2020-11-01 12:54:45 +0100

    Added Login form

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
+import Button from 'react-bootstrap/Button';
+
+class Login extends Component{
+	render(){
+
+		return (
+			<div className='Login'>
+				<FormGroup>
+					<FormLabel>User name</FormLabel>	
+						<FormControl type='text' placeholder='Enter email'/>
+					<FormLabel>User password</FormLabel>	
+						<FormControl type='password' placeholder='Enter email'/>
+					<FormLabel>Reapeat password</FormLabel>	
+						<FormControl type='password' placeholder='Enter email'/>
+					<FormLabel>Email</FormLabel>	
+						<FormControl type='email' placeholder='Enter email'/>
+					<Button type='submit'>Submit</Button>
+				</FormGroup>
+			</div>	
+		);
+	}
+}
+
+
+export default Login;
