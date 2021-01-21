9bf2f63358c5783689bf4b2389514358daa22600
K1C4Y <patryk.zajac.100@gmail.com>
2021-01-21 17:52:30 +0100

    Fixed text in login and register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,11 +8,12 @@ class Login extends Component {
   render() {
     return (
       <div className="Login">
+        <h2>Login</h2>
         <FormGroup>
           <FormLabel>User name</FormLabel>
           <FormControl type="text" placeholder="Enter email" />
           <FormLabel>User password</FormLabel>
-          <FormControl type="password" placeholder="Enter email" />
+          <FormControl type="password" placeholder="Enter password" />
           <Button type="submit">Submit</Button>
         </FormGroup>
       </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,13 +8,14 @@ class Register extends Component {
   render() {
     return (
       <div className="Register">
+        <h2>Register</h2>
         <FormGroup>
           <FormLabel>User name</FormLabel>
-          <FormControl type="text" placeholder="Enter email" />
+          <FormControl type="text" placeholder="Enter user name" />
           <FormLabel>User password</FormLabel>
-          <FormControl type="password" placeholder="Enter email" />
+          <FormControl type="password" placeholder="Enter password" />
           <FormLabel>Reapeat password</FormLabel>
-          <FormControl type="password" placeholder="Enter email" />
+          <FormControl type="password" placeholder="Confirm password" />
           <FormLabel>Email</FormLabel>
           <FormControl type="email" placeholder="Enter email" />
           <Button type="submit">Submit</Button>
